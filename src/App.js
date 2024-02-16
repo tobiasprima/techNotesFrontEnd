1b18200c6b33170afc8b5fc8afd69e8becd3f776
Tobias Prima <primatjahja@gmail.com>
2024-02-16 21:43:09 +0700

    feat: notes users routes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Public from './components/Public';
 import Login from './features/auth/Login';
 import DashLayout from './components/DashLayout';
 import Welcome from './features/auth/Welcome';
+import NotesList from './notes/NotesList';
+import UsersList from './users/UsersList';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout></Layout>}>
         <Route index element={<Public></Public>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='login' element={<Login></Login>}></Route>
         
-        <Route path='/dash' element={<DashLayout></DashLayout>}>
+        <Route path='dash' element={<DashLayout></DashLayout>}>
 
           <Route index element={<Welcome></Welcome>}></Route>
+          
+          <Route path='notes'>
+            <Route index element={<NotesList></NotesList>}></Route>
+          </Route>
+          
+          <Route path='users'>
+            <Route index element={<UsersList></UsersList>}></Route>
+          </Route>
 
         </Route>
       </Route>
