96fab2acc42ff8185fe98b343251b1735f080fce
Tobias Prima <primatjahja@gmail.com>
2024-02-20 14:07:55 +0700

    feat: routing to form

--- src/App.js
+++ src/App.js
@@ -6,24 +6,32 @@ import DashLayout from './components/DashLayout';
 import Welcome from './features/auth/Welcome';
 import NotesList from './features/notes/NotesList';
 import UsersList from './features/users/UsersList';
+import EditUser from './features/users/EditUser';
+import NewUserForm from './features/users/NewUserForm';
+import EditNote from './features/notes/EditNote';
+import NewNote from './features/notes/NewNote';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout></Layout>}>
-        <Route index element={<Public></Public>}></Route>
-        <Route path='login' element={<Login></Login>}></Route>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path='login' element={<Login />} />
         
-        <Route path='dash' element={<DashLayout></DashLayout>}>
+        <Route path='dash' element={<DashLayout />}>
 
-          <Route index element={<Welcome></Welcome>}></Route>
-          
-          <Route path='notes'>
-            <Route index element={<NotesList></NotesList>}></Route>
-          </Route>
+          <Route index element={<Welcome />} />
           
           <Route path='users'>
-            <Route index element={<UsersList></UsersList>}></Route>
+            <Route index element={<UsersList />} />
+            <Route path=':id' element={<EditUser />} />
+            <Route path='new' element={<NewUserForm />} />
+          </Route>
+
+          <Route path='notes'>
+            <Route index element={<NotesList />} />
+            <Route path=':id' element={<EditNote />} />
+            <Route path='new' element={<NewNote />} />
           </Route>
 
         </Route>
