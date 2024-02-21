baf44ca9b42d8c70f4100c3e301aa027f354f8b9
Tobias Prima <primatjahja@gmail.com>
2024-02-21 13:46:10 +0700

    feat: maintain subs with prefetch

--- src/features/users/EditUserForm.js
+++ src/features/users/EditUserForm.js
@@ -1,61 +1,58 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useUpdateUserMutation, useDeleteUserMutation } from './usersApiSlice';
-import { useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSave, faTrashCan } from '@fortawesome/free-solid-svg-icons';
-import { ROLES } from '../../config/roles';
-
-
+import { useState, useEffect } from "react"
+import { useUpdateUserMutation, useDeleteUserMutation } from "./usersApiSlice"
+import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons"
+import { ROLES } from "../../config/roles"
 
 const USER_REGEX = /^[A-z]{3,20}$/
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/
 
-
-const EditUserForm = (user) => {
+const EditUserForm = ({ user }) => {
 
     const [updateUser, {
         isLoading,
         isSuccess,
         isError,
-        error,
-    }] = useUpdateUserMutation();
+        error
+    }] = useUpdateUserMutation()
 
     const [deleteUser, {
         isSuccess: isDelSuccess,
         isError: isDelError,
-        error: delerror,
-    }] = useDeleteUserMutation();
+        error: delerror
+    }] = useDeleteUserMutation()
 
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
-
-    const [username, setUsername] = useState('');
-    const [validUsername, setValidUsername] = useState(false);
-    const [password, setPassword] = useState('');
-    const [validPassword, setValidPassword] = useState(false);
-    const [roles, setRoles] = useState(user.roles);
-    const [active , setActive] = useState(user.active);
+    const [username, setUsername] = useState(user.username)
+    const [validUsername, setValidUsername] = useState(false)
+    const [password, setPassword] = useState('')
+    const [validPassword, setValidPassword] = useState(false)
+    const [roles, setRoles] = useState(user.roles)
+    const [active, setActive] = useState(user.active)
 
     useEffect(() => {
         setValidUsername(USER_REGEX.test(username))
-    }, [username]) 
+    }, [username])
 
     useEffect(() => {
         setValidPassword(PWD_REGEX.test(password))
     }, [password])
-    
-    useEffect(() => {
-        if(isSuccess || isDelSuccess){
-            setUsername('');
-            setPassword('');
-            setRoles([]);
-            navigate('/dash/users');
-        }
-    }, [isSuccess, isDelSuccess, navigate]);
 
-    const onUsernameChanged = e => setUsername(e.target.value);
-    const onPasswordChanged = e => setPassword(e.target.value);
+    useEffect(() => {
+        console.log(isSuccess)
+        if (isSuccess || isDelSuccess) {
+            setUsername('')
+            setPassword('')
+            setRoles([])
+            navigate('/dash/users')
+        }
+
+    }, [isSuccess, isDelSuccess, navigate])
+
+    const onUsernameChanged = e => setUsername(e.target.value)
+    const onPasswordChanged = e => setPassword(e.target.value)
 
     const onRolesChanged = e => {
         const values = Array.from(
@@ -65,18 +62,18 @@ const EditUserForm = (user) => {
         setRoles(values)
     }
 
-    const onActiveChanged = () => setActive(prev => !prev);
+    const onActiveChanged = () => setActive(prev => !prev)
 
     const onSaveUserClicked = async (e) => {
-        if(password) {
-            await updateUser({id: user.id, username, password, roles, active});
+        if (password) {
+            await updateUser({ id: user.id, username, password, roles, active })
         } else {
-            await updateUser({id: user.id, username, roles, active});
+            await updateUser({ id: user.id, username, roles, active })
         }
     }
- 
-    const onDeleteUserClicked = async (e) => {
-        await deleteUser({id: user.id});
+
+    const onDeleteUserClicked = async () => {
+        await deleteUser({ id: user.id })
     }
 
     const options = Object.values(ROLES).map(role => {
@@ -184,5 +181,4 @@ const EditUserForm = (user) => {
 
     return content
 }
-
 export default EditUserForm
