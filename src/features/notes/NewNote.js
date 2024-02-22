7e28147d0528f32f6350764cd30f3b217ae98867
Tobias Prima <primatjahja@gmail.com>
2024-02-22 18:26:26 +0700

    feat: note forms

--- src/features/notes/NewNote.js
+++ src/features/notes/NewNote.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import NewNoteForm from './NewNoteForm';
+import { selectAllUsers } from '../users/usersApiSlice';
 
 const NewNote = () => {
-  return (
-    <div>NewNote</div>
-  )
+  const users = useSelector(selectAllUsers);
+
+  const content = users ? <NewNoteForm users={users} /> : <p>Loading ...</p>
+
+  return content
 }
 
-export default NewNote
+export default NewNote;
