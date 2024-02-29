511b0f9477e980af89764796d0e24a9b77c6f22a
Tobias Prima <primatjahja@gmail.com>
2024-02-29 12:18:44 +0800

    fix: fix backed base url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://techNotes-api.onrender.com',
+    baseUrl: 'https://technotes-api-g3mi.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
